3948b7aeb473615265645c3475dbbfb294d91dad
c-dance <zh.cecon@gmail.com>
2020-09-30 11:24:29 +0900

    docs modified

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,19 +19,19 @@ module.exports={
         ],
         sidebar: [
             {
-              title: 'vue',   // required
-              path: '/vue/',      // optional, link of the title, which should be an absolute path and must exist
+              title: 'Vue',   // required
+              path: '/Vue/',      // optional, link of the title, which should be an absolute path and must exist
               initialOpenGroupIndex: -1,
               collapsable: false, // optional, defaults to true
               sidebarDepth: 1,    // optional, defaults to 1
               children: [
-                '/vue/vue-cli'
+                '/Vue/vue-cli/'
               ]
             },
             {
-              title: 'vuepress',
-              path  : '/vuepress/',
-              children: [ /* */],
+              title: 'VuePress',
+              path  : '/VuePress/',
+              children: [ '/VuePress/*'],
               initialOpenGroupIndex: -1 // optional, defaults to 0, defines the index of initially opened subgroup
             },
           ]  
