5aad0cfb0211728dde6bef85d3e6d4855050aabc
joel <joel@joelui-MacBookPro.local>
2020-12-12 15:34:17 +0900

    docs modified

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,8 +16,8 @@ module.exports={
         ],
 
         sidebar : [ 
-          {path : "/git/", title:"Git", children:["/git/"], collapsable: true },
-          {path : "/adobe/", title : "ADOBE", childern : ["/acobe/"], collapsable:true}
+          {path : "../git", title:"Git", children:["/"], collapsable: true },
+          {path : "../adobe", title : "ADOBE", childern : ["/"], collapsable:true}
         ]
 }
 };
