2c25436f73c6e691a82f9d10cd9136baa3696d50
joel <joel@joelui-MacBookPro.local>
2020-12-12 19:13:51 +0900

    docs modified

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,8 +16,8 @@ module.exports={
         ],
         sidebar :[
          {path : "/git/", title:"Git", children:['/git/gitpage1','/git/gitpage2', '/git/gitignore'], collapsable:true },
-         {path : '/photoshop/', title:"PHOTOSHOP", children:['/photoshop/Ps-선택 툴','/photoshop/Ps-문자', '/photoshop/Ps-색 보정','/photoshop/Ps-사진배경'], collapsable:true},
-         {path : '/illust/', title : "ILLUST", children:[], collapsable:true}
+         {path : "/photoshop/", title:"PHOTOSHOP", children:['/photoshop/Ps-문자','/photoshop/Ps-사진배경', '/photoshop/Ps-색보정','/photoshop/Ps-선택 툴'], collapsable:true},
+         {path : "/illust/", title : "ILLUST", children:[''], collapsable:true}
         ]
 }
 }
