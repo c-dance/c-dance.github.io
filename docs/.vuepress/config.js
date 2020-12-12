fa80abfd3c9cb2b9824001ae45adc7194137ecd1
joel <joel@joelui-MacBookPro.local>
2020-12-12 16:52:35 +0900

    docs modified

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports={
           { text: 'About', link: '/About/' },
         ],
         sidebar :[
-         {path : "/git/", title:"Git", children:['/git/깃허브 페이지 만들기 [1]'], collapsable:true },
+         {path : "/git/", title:"Git", children:['/git/깃허브 페이지 만들기[1]','/git/Vuepress & 깃허브 페이지[2]'], collapsable:true },
          {path : "/adobe/", title : "ADOBE", childern : ['/adobe/[PS] 선택 툴'], collapsable:true}
         ]
 }
